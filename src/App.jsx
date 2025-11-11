b1a5b0d99c8e4fa2b5680b9f640d9dbab297e978
mukesh <mukeshvishwakarma251003@gmail.com>
2025-11-11 23:10:13 +0530

    chnage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/home/Home'
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Nav from './components/nav/Nav'
 import Footer from './components/Footer/Footer'
-import Shop from './pages/shop/shop'
+import Shop from './pages/shop/Shop'
 import Cart from './pages/cart/Cart'
 import Contact from './pages/contact/Contact'
 
